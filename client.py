28c4707f4cf1ff28c1d9af7ec1a0e90e5495d264
claySorrick <billiard117@yahoo.com>
2018-01-19 12:24:43 -0800

    send/recv continuously
    client and server can send and receive messages until 1 quits, then both
    close sockets and end program

--- client.py
+++ client.py
@@ -17,3 +17,21 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(client_socket)
 client_socket.connect((ADDRESS, PORT))
 print(client_socket)
+client_socket.send(bytes("HELLO", "utf-8"))
+print("Establishing connection...")
+
+while 1:
+	msg = client_socket.recv(512).decode()
+	if msg == "q":
+		print("server closed socket")
+		client_socket.close()
+		break;
+	print("Server: %s" % msg)
+	print("REPLY")
+	msg = input()
+	if msg == "q":
+		client_socket.send(bytes(msg, "utf-8"))
+		client_socket.close()
+		print("closing socket and exiting")
+		break;
+	client_socket.send(bytes(msg, "utf-8"))
--- server.py
+++ server.py
@@ -20,7 +20,20 @@ server_socket.bind((ADDRESS,PORT))
 print(server_socket)
 server_socket.listen(5)
 print("Server is listening on %s:%d" % (ADDRESS,PORT))
-
-
-client_socket = server_socket.accept()
+client_socket, address = server_socket.accept()
 print(client_socket)
+while 1:
+	msg = client_socket.recv(512).decode()
+	if msg == "q":
+		print("client closed socket")
+		client_socket.close()
+		break;
+	print("Client: %s" % msg)
+	print("REPLY")
+	msg = input()
+	if msg == "q":
+		client_socket.send(bytes(msg, "utf-8"))
+		client_socket.close()
+		print("closing socket and exiting")
+		break;
+	client_socket.send(bytes(msg, "utf-8"))
