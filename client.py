100f9e20329691c1ad028aa3a521391f828ce575
claySorrick <billiard117@yahoo.com>
2018-01-19 12:03:57 -0800

    connection made
    client and server see each other

--- client.py
+++ client.py
@@ -9,6 +9,11 @@ Created on Fri Jan 19 11:43:12 2018
 """
 import socket
 
+ADDRESS = "127.0.0.1"
+PORT = 6000
+
 print("CLIENT START")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(client_socket)
+client_socket.connect((ADDRESS, PORT))
 print(client_socket)
--- server.py
+++ server.py
@@ -10,6 +10,17 @@ Created on Fri Jan 19 11:43:15 2018
 """
 import socket
 
+ADDRESS = "127.0.0.1"
+PORT = 6000
+
 print("SERVER START")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(server_socket)
+server_socket.bind((ADDRESS,PORT))
+print(server_socket)
+server_socket.listen(5)
+print("Server is listening on %s:%d" % (ADDRESS,PORT))
+
+
+client_socket = server_socket.accept()
+print(client_socket)
